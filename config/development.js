d32d77001da29ffc5a45f82c41c8b7e46ed45026
vickey <vk@vickey.me>
2017-06-24 11:22:51 +0800

    更新了数据库信息

--- config/development.js
+++ config/development.js
@@ -3,15 +3,15 @@
  */
 
 module.exports = {
- //   env: 'development', //环境名称
-    env: 'production',
+    env: 'development', //环境名称
+ //   env: 'production',
     port: 80,         //服务端口号
 
-    database: 'popular',    //数据库地址
-    database_username:'',
-    database_password:'',
-    //database_host: 'ali.vickey.me', // 主机名
-    database_host: 'localhost', // 主机名
+    database: 'web_hr',    //数据库地址
+    database_username:'root',
+    database_password:'123456',
+    database_host: 'ali.vickey.me', // 主机名
+   // database_host: 'localhost', // 主机名
     database_port: 3306, // 端口号，MySQL默认3306
 
     redis_url:'',       //redis地址
