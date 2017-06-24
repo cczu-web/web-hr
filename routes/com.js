761d8de551505817b4b72837adc4edfd89d2441d
LuoLuo93 <1143775436@qq.com>
2017-06-24 18:01:10 +0800

    公司初步注册

--- routes/com.js
+++ routes/com.js
@@ -19,20 +19,44 @@ router
   })
 
 
-  //公司注册
-  //注册账号密码
+
+  //公司登陆
   .post('register',async (ctx, next) => {
-   console.log('123');
+   
+   let msg='';
    let user_phone=ctx.request.body.user_phone;
    let user_pwd=ctx.request.body.user_pwd;
-
-   let result = await comservice.userregisterValid(user_login, user_pwd);
+   
+   let result = await comservice.comregisterValid(user_phone, user_pwd);
    if(result=false){
         ctx.render('com_register.html');
+        console.log('1');
    }
    else{
+        console.log('2');
         ctx.render('index.html',
         msg=' 注册成功'+user_phone);
    }
   })
+
+//公司基初步注册
+.post('initial_register',async (ctx, next) => {
+  await comservice.com_user_initial_register(ctx);
+})
+
+
+
+//公司详细信息注册
+.post(('final_register',async (ctx, next) => {
+  await comservice.com_user_final_register(ctx);
+  
+})
+
+
+
+
+
+
+
+
   module.exports = router;
